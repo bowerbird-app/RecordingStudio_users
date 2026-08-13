ee48a6c6c76ba317b26470f3a320a8952d080f26
Lim Choon Yun (Marco) <marco.cylim@gmail.com>
2026-08-13 09:12:51 +0000

    test: align engine assertions with Rails

--- test/engine_test.rb
+++ test/engine_test.rb
@@ -4,10 +4,12 @@ require "test_helper"
 
 class EngineTest < Minitest::Test
   def test_engine_is_isolated_under_recording_studio_user
-    assert_equal RecordingStudioUser, RecordingStudioUser::Engine.isolated?
+    assert_predicate RecordingStudioUser::Engine, :isolated?
   end
 
   def test_engine_defines_only_profile_and_admin_routes
+    load File.expand_path("../config/routes.rb", __dir__)
+
     routes = RecordingStudioUser::Engine.routes.routes.map(&:name).compact
 
     assert_includes routes, "profile"
