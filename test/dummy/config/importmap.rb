b45d56684c6bdabc77986e2df04590078132004d
Lim Choon Yun (Marco) <marco.cylim@gmail.com>
2026-08-14 01:25:56 +0000

    Fix FlatPack alert controller imports

--- test/dummy/config/importmap.rb
+++ test/dummy/config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Pin FlatPack controllers
-pin_all_from FlatPack::Engine.root.join("app/javascript/flat_pack/controllers"), under: "controllers/flat_pack", to: "flat_pack/controllers", preload: false
+pin_all_from FlatPack::Engine.root.join("app/javascript/flat_pack/controllers"), under: "controllers", to: "flat_pack/controllers", preload: false
 pin "flat_pack/heroicons", to: "flat_pack/heroicons.js", preload: false
